daafbcd449ec0716ddff594047f1187e69ad560b
PEDRO33 <peter3fashion@gmail.com>
2017-10-09 18:39:22 -0700

    Login code updated - MainActivity changes

---
+++ app/src/main/java/doesangue/doesangue/me/mvp/main/MainModel.java
@@ -0,0 +1,8 @@
+package doesangue.doesangue.me.mvp.main;
+
+/**
+ * Created by JM on 09/10/2017.
+ */
+
+public class MainModel {
+}
---
+++ app/src/main/java/doesangue/doesangue/me/settings/SessionManager.java
@@ -0,0 +1,8 @@
+package doesangue.doesangue.me.settings;
+
+/**
+ * Created by JM on 09/10/2017.
+ */
+
+public class SessionManager {
+}
